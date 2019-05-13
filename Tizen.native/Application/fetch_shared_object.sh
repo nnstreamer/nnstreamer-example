fd18e9bef8bda7041e5b51902f567adf595fdb31
Parichay Kapoor <pk.kapoor@samsung.com>
2019-05-13 15:15:06 +0900

    [Tizen] Add basic example for NNstreamer for Tizen
    1. Verified loading of .so file provided by application by nnstreamer in Tizen
    2. Run a basic application to run a pipeline with custom filter
    
    tpk built using tizen studio, veified on Tizen 5.0 emulator
    
    V2:
    - Updated license to Apache 2.0
    - Added extra files to directly import this folder as a Tizen Studio
    project and build the app
    
    V3:
    - Download the custom filter so file from download.tizen.org
    
    V4:
    - Updated .gitignore
    
    V5:
    - Added a checker to verify the working of so file
    - Appended README for ARM builds
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ Tizen.native/Application/fetch_shared_object.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+##
+## @file fetch_shared_object.sh
+## @author Parichay Kapoor <pk.kapoor@samsung.com>
+## @date May 15th 2019
+## @brief fetch shared object of custom filter
+##
+
+
+# arch should be one of aarch64/x86_64/i686/armv7l for standard device
+# arch should be one of x86_64/i686 for emulator device
+arch="i686"
+if [ $# -ge 1 ]; then
+  arch=$1
+fi
+
+# arch should be one of standard/emulator
+device="emulator"
+if [ $# -ge 2 ]; then
+  device=$2
+fi
+
+# fix the version number and file to get
+version="0.1.3-5.1"
+rpmfile="nnstreamer-custom-filter-example-$version.$arch.rpm"
+sofile="libnnstreamer_customfilter_passthrough.so"
+
+# remove the file if existing
+if [ -f $rpmfile ]; then
+  rm $rpmfile
+fi
+wget "http://download.tizen.org/snapshots/tizen/unified/latest/repos/$device/packages/$arch/$rpmfile"
+
+# extract the so from the rpm file and move to correct location
+sofilepath=$(rpm2cpio $rpmfile | cpio -t | grep $sofile)
+rpm2cpio $rpmfile | cpio -id $sofilepath
+chmod 755 $sofilepath
+mv -f $sofilepath ./res/.
+
+
+# clean the remaining files
+rm -rf ./usr
+rm -rf $rpmfile
---
+++ Tizen.native/Application/src/main.c
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @file  main.c
+ * @date  9 May 2019
+ * @brief Application to test nnstreamer
+ * @see   http://github.com/nnsuite/nnstreamer
+ * @author  Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug   No known bugs except for NYI items
+ *
+ */
+
+#include <app.h>
+#include <dlog.h>
+#include <dlfcn.h>
+#include <Elementary.h>
+#include <tizen_error.h>
+#include <nnstreamer/tizen-api.h>
+
+#include "main.h"
+
+/**
+ * @brief Macro for buffer sizes
+ */
+#define FILE_BUFFER_SIZE 256
+#define FILENAME_BUFFER_SIZE 1024
+
+/**
+ * @brief function declaration for verifying so file
+ */
+typedef void *(*_nns_custom_init_func) (const void *);
+struct NNStreamer_custom_class
+{
+  _nns_custom_init_func initfunc;
+};
+
+/**
+ * @brief Compare contents of two binary files.
+ */
+int
+compare_output_files (const char *file1, const char *file2)
+{
+  FILE *fp1, *fp2;
+  char tmp1[FILE_BUFFER_SIZE], tmp2[FILE_BUFFER_SIZE];
+  size_t n1, n2;
+
+  fp1 = fopen (file1, "rb");
+  if (fp1 == NULL) {
+    return -1;
+  }
+  fp2 = fopen (file2, "rb");
+  if (fp2 == NULL) {
+    fclose (fp1);
+    return -1;
+  }
+
+  do {
+    n1 = fread (tmp1, FILE_BUFFER_SIZE, 1, fp1);
+    n2 = fread (tmp2, FILE_BUFFER_SIZE, 1, fp2);
+
+    if (n1 != n2 || memcmp (tmp1, tmp2, n1)) {
+      fclose (fp1);
+      fclose (fp2);
+      return -1;
+    }
+
+  } while (n1);
+
+  fclose (fp1);
+  fclose (fp2);
+  return TIZEN_ERROR_NONE;
+}
+
+/**
+ * @brief verify that the so file can be loaded properly
+ */
+int
+verify_so_file (const char *filename)
+{
+  void *dlhandle;
+  struct NNStreamer_custom_class *nnst_class;
+  char *dlsym_error;
+
+  dlhandle = dlopen (filename, RTLD_NOW);
+  if (dlhandle == NULL) {
+    dlog_print (DLOG_ERROR, "NNStreamerApp", "Unable to open so file.");
+    return -1;
+  }
+
+  dlerror ();
+  nnst_class =
+      *((struct NNStreamer_custom_class **) dlsym (dlhandle,
+          "NNStreamer_custom"));
+  dlsym_error = dlerror ();
+  if (dlsym_error) {
+    dlog_print (DLOG_ERROR, "NNStreamerApp",
+        "Unable to read symbol from so file with error %s", dlsym_error);
+    dlclose (dlhandle);
+    return -1;
+  }
+
+  dlclose (dlhandle);
+  return TIZEN_ERROR_NONE;
+}
+
+/**
+ * @brief Main function to test run SO file of the application.
+ */
+int
+main (int argc, char *argv[])
+{
+  char direct_file[FILENAME_BUFFER_SIZE];
+  char passthrough_file[FILENAME_BUFFER_SIZE];
+  char custom_filter_file[FILENAME_BUFFER_SIZE];
+  char pipeline[FILENAME_BUFFER_SIZE];
+  void *handle;
+  int status;
+  nns_pipeline_state_e state;
+
+  snprintf (custom_filter_file, FILENAME_BUFFER_SIZE,
+      "%s/libnnstreamer_customfilter_passthrough.so", app_get_resource_path ());
+  snprintf (direct_file, FILENAME_BUFFER_SIZE, "%s/testcase01.direct.log",
+      app_get_shared_data_path ());
+  snprintf (passthrough_file, FILENAME_BUFFER_SIZE,
+      "%s/testcase01.passthrough.log", app_get_shared_data_path ());
+  snprintf (pipeline, FILENAME_BUFFER_SIZE,
+      "videotestsrc num-buffers=1 ! video/x-raw,format=RGB,width=280,height=40,framerate=0/1 ! videoconvert ! video/x-raw, format=RGB ! tensor_converter ! tee name=t ! queue ! tensor_filter framework=\"custom\" model=\"%s\" input=\"3:280:40\" inputtype=\"uint8\" output=\"3:280:40\" outputtype=\"uint8\" ! multifilesink location=\"%s\" sync=true t. ! queue ! multifilesink location=\"%s\" sync=true",
+      custom_filter_file, passthrough_file, direct_file);
+
+  status = verify_so_file (custom_filter_file);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  status = nns_pipeline_construct (pipeline, &handle);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  status = nns_pipeline_start (handle);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  sleep (1);
+  status = nns_pipeline_getstate (handle, &state);
+  if (status != TIZEN_ERROR_NONE || state != NNS_PIPELINE_PLAYING) {
+    return -1;
+  }
+  status = nns_pipeline_stop (handle);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  sleep (1);
+  status = nns_pipeline_getstate (handle, &state);
+  if (status != TIZEN_ERROR_NONE || state != NNS_PIPELINE_PAUSED) {
+    return -1;
+  }
+
+  status = nns_pipeline_destroy (handle);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  status = compare_output_files (direct_file, passthrough_file);
+  if (status != TIZEN_ERROR_NONE) {
+    return -1;
+  }
+
+  dlog_print (DLOG_INFO, "NNStreamerApp", "Successfully ran the pipeline");
+  return 0;
+}
