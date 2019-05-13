13eb3fa75cb1d44e493cc69118f1c9de77e16fe6
Parichay Kapoor <pk.kapoor@samsung.com>
2019-05-13 15:15:06 +0900

    [Tizen] Add basic example for NNstreamer for Tizen
    1. Verified loading of .so file provided by application by nnstreamer in Tizen
    2. Run a basic application to run a pipeline with custom filter
    
    tpk built using tizen studio, veified on Tizen 5.0 emulator
    
    V2:
    - Updated license to Apache 2.0
    - Added extra files to directly import this folder as a Tizen Studio
    project and build the app
    
    V3:
    - Download the custom filter so file from download.tizen.org
    
    V4:
    - Updated .gitignore
    
    V5:
    - Added a checker to verify the working of so file
    - Appended README for ARM builds
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ Tizen.native/Application/inc/main.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @file  main.h
+ * @date  9 May 2019
+ * @brief Application to test nnstreamer
+ * @see   http://github.com/nnsuite/nnstreamer
+ * @author  Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug   No known bugs except for NYI items
+ *
+ */
+
+#if !defined(_MAIN_H_)
+#define _MAIN_H_
+
+/**
+ * @brief Macro for PACKAGE name
+ */
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.application"
+#endif
+
+#endif
---
+++ Tizen.native/README.md
@@ -0,0 +1,17 @@
+# Tizen native (c) NNStreamer Example Application
+
+Provides a basic tizen native application to verify that platform libraries can
+load shared objects of application. The application contains a neural network
+implemented as a custom filter packaged inside its tpk.
+
+The application can be built and run on device/emulator using Tizen Studio.
+The applicaiton was tested with Tizen Emulator with Tizen 5.0 mobile version.
+
+Note: use `fetch_shared_object.sh` script to download so file into the `res`
+of the application. Set the architecture and device appropriately.
+Specify armv7l as the architecture for ARM builds.
+
+Note: use the nnstreamer rpm files from the same build as custom filter. Using
+packages from different builds can cause compatibility issues and might not
+work together.For ARM devices, the packages can be downloaded from
+http://download.tizen.org/snapshots/tizen/unified/latest/repos/standard/packages/armv7l/
