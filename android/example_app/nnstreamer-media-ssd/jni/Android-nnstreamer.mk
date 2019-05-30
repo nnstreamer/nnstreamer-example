b3cfed20d294777723e916fec30ce5139dc9d28a
Dongju Chae <dongju.chae@samsung.com>
2019-05-30 20:52:09 +0900

    [Android] add SSD example using Android MediaCodec (AMC) source
    Android application example (tf-lite ssd object detection)
    It contains small changes of the existing object detection app
    
    Signed-off-by: Dongju Chae <dongju.chae@samsung.com>

---
+++ android/example_app/nnstreamer-media-ssd/jni/Android-nnstreamer.mk
@@ -0,0 +1,43 @@
+#------------------------------------------------------
+# nnstreamer
+#------------------------------------------------------
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := nnstreamer
+
+ifndef NNSTREAMER_ROOT
+$(error NNSTREAMER_ROOT is not defined!)
+endif
+
+include $(NNSTREAMER_ROOT)/jni/nnstreamer.mk
+
+NNSTREAMER_SOURCE_AMC_SRCS := \
+    $(NNSTREAMER_EXT_HOME)/android_source/gstamcsrc.c \
+    $(NNSTREAMER_EXT_HOME)/android_source/gstamcsrc_looper.cc
+
+LOCAL_SRC_FILES := \
+    $(NNSTREAMER_COMMON_SRCS) \
+    $(NNSTREAMER_PLUGINS_SRCS) \
+    $(NNSTREAMER_FILTER_TFLITE_SRCS) \
+    $(NNSTREAMER_SOURCE_AMC_SRCS)
+
+LOCAL_C_INCLUDES := \
+    $(NNSTREAMER_INCLUDES)
+
+# common headers (gstreamer, glib)
+LOCAL_C_INCLUDES += \
+    $(GSTREAMER_ROOT)/include/gstreamer-1.0 \
+    $(GSTREAMER_ROOT)/include/glib-2.0 \
+    $(GSTREAMER_ROOT)/lib/glib-2.0/include \
+    $(GSTREAMER_ROOT)/include
+
+# common headers (tensorflow-lite)
+LOCAL_C_INCLUDES += \
+    $(TF_LITE_INCLUDES)
+
+LOCAL_CFLAGS += -O2 -DVERSION=\"$(NNSTREAMER_VERSION)\"
+LOCAL_CXXFLAGS += -std=c++11 -O2 -DVERSION=\"$(NNSTREAMER_VERSION)\"
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ android/example_app/nnstreamer-media-ssd/jni/Android.mk
@@ -0,0 +1,46 @@
+LOCAL_PATH := $(call my-dir)
+
+ifndef GSTREAMER_ROOT_ANDROID
+$(error GSTREAMER_ROOT_ANDROID is not defined!)
+endif
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/armv7
+else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm64
+else
+$(error Target arch ABI not supported: $(TARGET_ARCH_ABI))
+endif
+
+#------------------------------------------------------
+# nnstreamer jni common for external libs
+#------------------------------------------------------
+include $(LOCAL_PATH)/../../common/jni/nnstreamer-jni-common.mk
+
+#------------------------------------------------------
+# nnstreamer
+#------------------------------------------------------
+include $(LOCAL_PATH)/Android-nnstreamer.mk
+
+#------------------------------------------------------
+# nnstreamer example
+#------------------------------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := nnstreamer-jni
+LOCAL_SRC_FILES := nnstreamer-jni.c nnstreamer-ex.cpp
+LOCAL_STATIC_LIBRARIES := nnstreamer tensorflow-lite
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog -landroid -lmediandk -lOpenMAXAL
+
+include $(BUILD_SHARED_LIBRARY)
+
+GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
+include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
+# add necessary gstreamer plugins
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_SYS) cairo
+GSTREAMER_EXTRA_DEPS      := gstreamer-video-1.0 gstreamer-audio-1.0 gobject-2.0
+# cairo graphics library is used to display detected objects in this example.
+# (cairooverlay, https://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-good/html/gst-plugins-good-plugins-cairooverlay.html)
+GSTREAMER_EXTRA_LIBS      := -liconv -lcairo
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
