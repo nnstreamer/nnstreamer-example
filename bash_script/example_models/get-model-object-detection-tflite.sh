eb26a54ef3de4f89e64c7eb38761c4a29e358fb5
Junsang Mo <junsang.mo@samsung.com>
2019-10-10 21:11:26 +0900

    [CodeClean] merge get-model scripts Delete the existing get-model scripts and merge it into model-get.sh Identify models by arguments Add symlink Update meson.build script
    Signed-off-by: Junsang Mo <junsang.mo@samsung.com>

--- bash_script/example_models/get-model-object-detection-tflite.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p tflite_model
-cd tflite_model
-wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/ssd_mobilenet_v2_coco.tflite
-wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/coco_labels_list.txt
-wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/box_priors.txt
--- bash_script/example_models/get-model-speech-command.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p speech_model
-cd speech_model
-wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/conv_actions_tflite.zip
-unzip conv_actions_tflite.zip
-rm conv_actions_tflite.zip
