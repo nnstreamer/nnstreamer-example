053f2545ac9e82c4eba38d155b200459dd00392e
HyoungJoo Ahn <hello.ahn@samsung.com>
2019-10-11 19:33:56 +0900

    [Tizne/Native] upload sample app of nnstreamer single API
    this is a simple example of nnstreamer single API
    
    Signed-off-by: HyoungJoo Ahn <hello.ahn@samsung.com>

---
+++ Tizen.native/SingleSample/inc/nnstreamer-sample-single.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ */
+
+/**
+ * @file nnstreamer-sample-single.h
+ * @date 11 October 2019
+ * @brief NNStreamer/C-API SingleShot example.
+ * @see	https://github.com/nnsuite/nnstreamer
+ * @author HyoungJoo Ahn <hello.ahn@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#ifndef __NNSTREAMER_SAMPLE_SINGLE_H__
+#define __NNSTREAMER_SAMPLE_SINGLE_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#include <nnstreamer.h>
+#include <nnstreamer-single.h>
+#include <app_resource_manager.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "NNStreamer_Sample"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.nnstreamer-sample-single"
+#endif
+
+#endif /* __NNSTREAMER_SAMPLE_SINGLE_H__ */
