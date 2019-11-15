e824e2f6f53a4f9750bc2cbcbe235258b7714943
Jihoon Lee <ulla4571@gmail.com>
2019-11-15 15:08:19 +0900

    [Bash/Sample] Add sample code to run tflite image segmentation
    Add sample bash script which get image from webcam
    and paint regions of objects.
    
    Signed-off-by: niklasjang <niklasjang@gmail.com>
    Signed-off-by: Jihoon Lee <ulla4571@gmail.com>

---
+++ bash_script/example_image_segmentation_tensorflow_lite/gst-launch-image-segmentation-tflite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+gst-launch-1.0 -v \
+      videomixer name=mix sink_0::alpha=0.7 sink_1::alpha=0.6 ! videoconvert ! autovideosink \
+      v4l2src ! decodebin ! videoconvert ! videoscale ! \
+      video/x-raw,format=RGB,width=257,height=257 ! tee name=t \
+      t. ! queue ! mix. \
+      t. ! queue ! tensor_converter ! \
+      tensor_transform mode=arithmetic option=typecast:float32,div:255.0 ! \
+      tensor_filter framework=tensorflow-lite model=tflite_img_segment_model/deeplabv3_257_mv_gpu.tflite ! \
+      tensor_decoder mode=image_segment option1=tflite-deeplab ! mix.
