5d3a400f681bbb688c1aa9d126b9b7790a14bafb
gichan-jang <gichan2.jang@samsung.com>
2019-11-28 15:03:48 +0900

    [WIP][Tflite/example] Insert buffer to src pad and read from sink
    1) Insert user input to source pad.
    2) Read new-data from sink pad.
    
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

--- native/example_text_classification/nnstreamer_example_text_classification_tflite.c
+++ native/example_text_classification/nnstreamer_example_text_classification_tflite.c
@@ -111,6 +111,21 @@ static void
 new_data_cb (GstElement * element, GstBuffer * buffer, gpointer user_data)
 {
   g_print("New data is arrived..\n");
+
+  GstMemory *mem_res;
+  GstMapInfo info_res;
+  gchar *converted_val;
+
+  mem_res = gst_buffer_get_memory (buffer, 0);
+  g_assert (gst_memory_map (mem_res, &info_res, GST_MAP_READ));
+  converted_val =  (gchar *)info_res.data;
+
+  g_print("Sink pad data : %s\n",converted_val);
+
+  gst_memory_unmap (mem_res, &info_res);
+  gst_memory_unref (mem_res);
+
+
 }
 
 /**
@@ -145,45 +160,45 @@ load_model_files (app_data_s * app)
 
 	g_free (contents);
   } else {
-	g_critical ("Failed to load label file.");
-	failed = TRUE;
+    g_critical ("Failed to load label file.");
+    failed = TRUE;
 	goto error;
   }
 
   /* load dictionary */
   if (g_file_get_contents (vocab, &contents, NULL, NULL)) {
-	gchar **dics = g_strsplit (contents, "\n", -1);
-	guint dics_len = g_strv_length (dics);
-	guint i;
+    gchar **dics = g_strsplit (contents, "\n", -1);
+    guint dics_len = g_strv_length (dics);
+    guint i;
 
-	/* init hash table */
-	app->words = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
-	g_assert (app->words);
+    /* init hash table */
+    app->words = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+    g_assert (app->words);
 
-	for (i = 0; i < dics_len; i++) {
-	  /* get word and index */
-	  gchar **word = g_strsplit (dics[i], " ", 2);
+    for (i = 0; i < dics_len; i++) {
+      /* get word and index */
+      gchar **word = g_strsplit (dics[i], " ", 2);
 
-	  if (g_strv_length (word) == 2) {
-		gint word_index = (gint) g_ascii_strtoll (word[1], NULL, 10);
+      if (g_strv_length (word) == 2) {
+      gint word_index = (gint) g_ascii_strtoll (word[1], NULL, 10);
 
-		gpointer key = g_strdup (word[0]);
-		gpointer value = GINT_TO_POINTER (word_index);
+      gpointer key = g_strdup (word[0]);
+      gpointer value = GINT_TO_POINTER (word_index);
 
-		g_assert (g_hash_table_insert (app->words, key, value));
+      g_assert (g_hash_table_insert (app->words, key, value));
 	  } else {
-		g_print("Failed to parse voca.\n");
+		  g_print("Failed to parse voca.\n");
 	  }
 
 	  g_strfreev (word);
 	  }
 
-	g_strfreev (dics);
-	g_free (contents);
+    g_strfreev (dics);
+    g_free (contents);
   } else {
-	g_critical ("Failed to load dictionary file.");
-	failed = TRUE;
-	goto error;
+    g_critical ("Failed to load dictionary file.");
+    failed = TRUE;
+    goto error;
   }
 
   gint test_val = GPOINTER_TO_INT(g_hash_table_lookup(app->words, "radar"));
@@ -204,13 +219,16 @@ load_model_files (app_data_s * app)
 static void
 handle_input_string (app_data_s * app)
 {
+  GstBuffer *buf;
+  GstElement *appsrc;
   gchar **tokens;
   gint tokens_len, i, value, start, unknown, pad;
-  static gfloat buf[MAX_SENTENCE_LENGTH]={0.0, };
+  static gfloat float_array[MAX_SENTENCE_LENGTH]={0.0, };
 
   /* Get user input */
   gchar sentence[MAX_SENTENCE_LENGTH] = { 0, };
   g_print("Please enter your movie review : ");
+
   if(fgets (sentence, sizeof (sentence), stdin)) {
 	  g_print("Input : %s",sentence);
   }
@@ -219,20 +237,28 @@ handle_input_string (app_data_s * app)
   pad = GPOINTER_TO_INT (g_hash_table_lookup (app->words, "<PAD>"));
   unknown = GPOINTER_TO_INT (g_hash_table_lookup (app->words, "<UNKNOWN>"));
   
-  buf[0] = (gfloat) start;
+  float_array[0] = (gfloat) start;
   
-  tokens = g_strsplit_set(sentence," \n",MAX_SENTENCE_LENGTH);
+  tokens = g_strsplit_set(sentence," \n\t",MAX_SENTENCE_LENGTH);
   tokens_len = g_strv_length(tokens) - 1;
 
   for(i=0;i<tokens_len;i++) {
-	value = GPOINTER_TO_INT(g_hash_table_lookup(app->words, tokens[i]));
-	buf[i+1]= (gfloat) (value>0?value:unknown);
-	g_print("Converted float array : %lf \n",buf[i+1]);
+    value = GPOINTER_TO_INT(g_hash_table_lookup(app->words, tokens[i]));
+    float_array[i+1]= (gfloat) (value>0?value:unknown);
+    g_print("Converted float array : %.1lf \n",float_array[i+1]);
   }
   while(i<MAX_SENTENCE_LENGTH) {
-	buf[i++] = (gfloat) pad;
+	  float_array[i++] = (gfloat) pad;
   }
+  
+  /* get appsrc element using name */
+  appsrc = gst_bin_get_by_name (GST_BIN (app->pipeline), "appsrc");
+  g_assert (appsrc != NULL);
 
+  buf = gst_buffer_new_wrapped (sentence, MAX_SENTENCE_LENGTH);
+  g_assert(buf);
+  
+  g_assert (gst_app_src_push_buffer (GST_APP_SRC (appsrc), buf) == GST_FLOW_OK);
 }
 
 /**
@@ -260,7 +286,7 @@ main (int argc, char **argv)
    pipeline = 
        g_strdup_printf 
        ("appsrc name=appsrc caps=text/x-raw,format=utf8 ! "
-          "tensor_converter input-dim = 30 ! tensor_sink name=tensor_sink");
+        "tensor_converter input-dim = 256 ! tensor_sink name=tensor_sink");
   
   app->pipeline = gst_parse_launch (pipeline, NULL);
   g_free (pipeline);
@@ -280,17 +306,20 @@ main (int argc, char **argv)
 
   /* tensor sink signal : new data callback */
   g_signal_connect (element, "new-data",(GCallback) new_data_cb, NULL);
+  gst_object_unref (element);
 
-  /* get user input */
-  handle_input_string (app);
+
 
   /* Start playing */
   gst_element_set_state (app->pipeline, GST_STATE_PLAYING);
 
+  /* get user input */
+  handle_input_string (app);
+  
   g_usleep (200 * 1000);
 
   /* stop the pipeline */
-  gst_element_set_state (app->pipeline, GST_STATE_NULL);
+  //gst_element_set_state (app->pipeline, GST_STATE_NULL);
 
 
  /* close app */
