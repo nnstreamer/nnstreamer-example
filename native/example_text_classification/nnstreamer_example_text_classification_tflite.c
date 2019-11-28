3c11df5ff81800b8202d0f256c2cbb7510ad1ab7
gichan-jang <gichan2.jang@samsung.com>
2019-11-28 10:21:18 +0900

    [WIP][Tflite/example] Add to get user input and preprocess
    Get user input from stdin and convert float array to match with model input dimension
    
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

--- native/example_text_classification/nnstreamer_example_text_classification_tflite.c
+++ native/example_text_classification/nnstreamer_example_text_classification_tflite.c
@@ -12,6 +12,8 @@
 #include <gst/gst.h>
 #include <gst/app/app.h>
 
+#define MAX_SENTENCE_LENGTH 256
+
 /**
  * @brief Data structure for app.
  */
@@ -45,61 +47,61 @@ load_model_files (app_data_s * app)
 
   /* check the model files */
   if (!g_file_test (model, G_FILE_TEST_EXISTS) ||
-      !g_file_test (label, G_FILE_TEST_EXISTS) ||
-      !g_file_test (vocab, G_FILE_TEST_EXISTS)) {
-    g_critical ("Failed to get model files.");
-    failed = TRUE;
-    goto error;
+	  !g_file_test (label, G_FILE_TEST_EXISTS) ||
+	  !g_file_test (vocab, G_FILE_TEST_EXISTS)) {
+	g_critical ("Failed to get model files.");
+	failed = TRUE;
+	goto error;
   }
 
   /* load label file */
   if (g_file_get_contents (label, &contents, NULL, NULL)) {
-    app->labels = g_strsplit (contents, "\n", -1);
+	app->labels = g_strsplit (contents, "\n", -1);
     
-    g_print("label 1 : %s \n", app->labels[0]);
-    g_print("label 2 : %s \n", app->labels[1]);
+	g_print("label 1 : %s \n", app->labels[0]);
+	g_print("label 2 : %s \n", app->labels[1]);
 
-    g_free (contents);
+	g_free (contents);
   } else {
-    g_critical ("Failed to load label file.");
-    failed = TRUE;
-    goto error;
+	g_critical ("Failed to load label file.");
+	failed = TRUE;
+	goto error;
   }
 
   /* load dictionary */
   if (g_file_get_contents (vocab, &contents, NULL, NULL)) {
-    gchar **dics = g_strsplit (contents, "\n", -1);
-    guint dics_len = g_strv_length (dics);
-    guint i;
+	gchar **dics = g_strsplit (contents, "\n", -1);
+	guint dics_len = g_strv_length (dics);
+	guint i;
 
-    /* init hash table */
-    app->words = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
-    g_assert (app->words);
+	/* init hash table */
+	app->words = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+	g_assert (app->words);
 
-    for (i = 0; i < dics_len; i++) {
-      /* get word and index */
-      gchar **word = g_strsplit (dics[i], " ", 2);
+	for (i = 0; i < dics_len; i++) {
+	  /* get word and index */
+	  gchar **word = g_strsplit (dics[i], " ", 2);
 
-      if (g_strv_length (word) == 2) {
-        gint word_index = (gint) g_ascii_strtoll (word[1], NULL, 10);
+	  if (g_strv_length (word) == 2) {
+		gint word_index = (gint) g_ascii_strtoll (word[1], NULL, 10);
 
-        gpointer key = g_strdup (word[0]);
-        gpointer value = GINT_TO_POINTER (word_index);
+		gpointer key = g_strdup (word[0]);
+		gpointer value = GINT_TO_POINTER (word_index);
 
-        g_assert (g_hash_table_insert (app->words, key, value));
-      } else {
-        g_print("Failed to parse voca.\n");
-      }
+		g_assert (g_hash_table_insert (app->words, key, value));
+	  } else {
+		g_print("Failed to parse voca.\n");
+	  }
 
-      g_strfreev (word);
-    }
+	  g_strfreev (word);
+	}
 
-    g_strfreev (dics);
-    g_free (contents);
+	g_strfreev (dics);
+	g_free (contents);
   } else {
-    g_critical ("Failed to load dictionary file.");
-    failed = TRUE;
-    goto error;
+	g_critical ("Failed to load dictionary file.");
+	failed = TRUE;
+	goto error;
   }
 
 gint test_val = GPOINTER_TO_INT(g_hash_table_lookup(app->words, "radar"));
@@ -115,12 +117,49 @@ error:
 }
 
 /**
+ * @brief Function to handle input string.
+ */
+static void
+handle_input_string (app_data_s * app)
+{
+  gchar **tokens;
+  gint tokens_len, i, value, start, unknown, pad;
+  static gfloat buf[MAX_SENTENCE_LENGTH]={0.0, };
+
+  /* Get user input */
+  gchar sentence[MAX_SENTENCE_LENGTH] = { 0, };
+  g_print("Please enter your movie review : ");
+  if(fgets (sentence, sizeof (sentence), stdin)) {
+	  g_print("Input : %s",sentence);
+  }
+  
+  start = GPOINTER_TO_INT (g_hash_table_lookup (app->words, "<START>"));
+  pad = GPOINTER_TO_INT (g_hash_table_lookup (app->words, "<PAD>"));
+  unknown = GPOINTER_TO_INT (g_hash_table_lookup (app->words, "<UNKNOWN>"));
+  
+  buf[0] = (gfloat) start;
+  
+  tokens = g_strsplit_set(sentence," \n",MAX_SENTENCE_LENGTH);
+  tokens_len = g_strv_length(tokens) - 1;
+
+  for(i=0;i<tokens_len;i++) {
+    value = GPOINTER_TO_INT(g_hash_table_lookup(app->words, tokens[i]));
+    buf[i+1]= (gfloat) (value>0?value:unknown);
+    g_print("Converted float array : %lf \n",buf[i+1]);
+  }
+  while(i<MAX_SENTENCE_LENGTH) {
+    buf[i++] = (gfloat) pad;
+  }
+
+}
+
+/**
  * @brief Main function.
  */
 int
 main (int argc, char **argv)
 {
-    app_data_s *app;
+	app_data_s *app;
 
   /* init gstreamer */
   gst_init (&argc, &argv);
@@ -131,6 +170,9 @@ main (int argc, char **argv)
 
   /* load model files */
   g_assert (load_model_files (app));
+
+  /* get user input */
+  handle_input_string (app);
 
   g_free (app->model_file);
   g_strfreev (app->labels);
