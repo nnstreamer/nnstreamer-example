a7b01ba0ab94de33fbf01f2b6ef8f99d74955452
Jaeyun <jy1210.jung@samsung.com>
2019-12-20 15:15:59 +0900

    [Android/SNAP] sample code for SNAP
    Update sample code to execute SNAP model.
    (native API with pipeline description)
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- android/example_app/capi-sample/src/main/jni/nnstreamer-native-sample.c
+++ android/example_app/capi-sample/src/main/jni/nnstreamer-native-sample.c
@@ -59,20 +59,123 @@ sample_sink_cb (const ml_tensors_data_h data, const ml_tensors_info_h info, void
 }
 
 /**
+ * @brief Simple pipeline example.
+ */
+static void
+run_simple_pipeline (void)
+{
+  ml_pipeline_h pipe = NULL;
+  ml_pipeline_sink_h sink = NULL;
+  int status = ML_ERROR_NONE;
+
+  /* pipeline description */
+  const char description[] = "videotestsrc ! videoconvert ! video/x-raw,format=RGB ! tensor_converter ! tensor_sink name=sinkx";
+
+  LOGI ("Start to run NNStreamer pipeline example.");
+
+  /* construct a pipeline */
+  status = ml_pipeline_construct (description, NULL, NULL, &pipe);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to construct the pipeline.");
+    goto done;
+  }
+
+  /* register sink callback */
+  status = ml_pipeline_sink_register (pipe, "sinkx", sample_sink_cb, NULL, &sink);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to construct the pipeline.");
+    goto done;
+  }
+
+  /* start the pipeline */
+  status = ml_pipeline_start (pipe);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to start the pipeline.");
+    goto done;
+  }
+
+  /* sleep to run pipeline for a while */
+  sleep (5);
+
+done:
+  /* destroy the pipeline */
+  if (ml_pipeline_destroy (pipe) != ML_ERROR_NONE) {
+    LOGE ("Failed to destroy pipeline handle.");
+  }
+}
+
+/**
+ * @brief SNAP example.
+ */
+static void
+run_snap_example (void)
+{
+  ml_pipeline_h pipe = NULL;
+  ml_pipeline_sink_h sink = NULL;
+  int status = ML_ERROR_NONE;
+
+  /**
+   * Pipeline with SNAP
+   *
+   * Set custom properties in tensor-filter.
+   *
+   * GPU: "custom=ModelFWType:CAFFE,ExecutionDataType:FLOAT32,ComputingUnit:GPU,GpuCacheSource:/sdcard/nnstreamer/"
+   * Denote GPU cache path. (GpuCacheSource:cache-path)
+   * It takes a time with GPU option in the first run.
+   *
+   * CPU: "custom=ModelFWType:CAFFE,ExecutionDataType:FLOAT32,ComputingUnit:CPU"
+   * Set CPU thread count. (default 4. e.g., CpuThreadCount:5 to set the count 5.)
+   */
+  const char description[] = "videotestsrc ! videoconvert ! videoscale ! "
+            "video/x-raw,format=RGB,width=224,height=224,framerate=(fraction)30/1 ! "
+            "tensor_converter ! tensor_transform mode=typecast option=float32 ! "
+            "tensor_filter framework=snap "
+                "model=/sdcard/nnstreamer/snap_data/prototxt/squeezenet.prototxt,"
+                    "/sdcard/nnstreamer/snap_data/model/squeezenet.caffemodel "
+                "input=3:224:224:1 inputtype=float32 inputlayout=NHWC inputname=data "
+                "output=1:1:1000:1 outputtype=float32 outputlayout=NCHW outputname=prob "
+                "custom=ModelFWType:CAFFE,ExecutionDataType:FLOAT32,ComputingUnit:CPU ! "
+            "tensor_sink name=sinkx";
+
+  LOGI ("Start to run NNStreamer pipeline with SNAP.");
+
+  /* construct a pipeline */
+  status = ml_pipeline_construct (description, NULL, NULL, &pipe);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to construct the pipeline.");
+    goto done;
+  }
+
+  /* register sink callback */
+  status = ml_pipeline_sink_register (pipe, "sinkx", sample_sink_cb, NULL, &sink);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to construct the pipeline.");
+    goto done;
+  }
+
+  /* start the pipeline */
+  status = ml_pipeline_start (pipe);
+  if (status != ML_ERROR_NONE) {
+    LOGE ("Failed to start the pipeline.");
+    goto done;
+  }
+
+  /* sleep to run pipeline for a while */
+  sleep (10);
+
+done:
+  /* destroy the pipeline */
+  if (ml_pipeline_destroy (pipe) != ML_ERROR_NONE) {
+    LOGE ("Failed to destroy pipeline handle.");
+  }
+}
+
+/**
  * @brief Native method to run sample pipeline with NNStreamer.
  */
 jboolean
 Java_org_nnsuite_nnstreamer_sample_MainActivity_nativeRunSample (JNIEnv * env, jobject thiz)
 {
-  ml_pipeline_h pipe;
-  ml_pipeline_sink_h sink;
-  int status;
-
-  /**
-   * Pipeline description.
-   */
-  const char description[] = "videotestsrc ! videoconvert ! video/x-raw,format=RGB ! tensor_converter ! tensor_sink name=sinkx";
-
   LOGI ("Start to run NNStreamer C-API sample.");
 
   /**
@@ -84,47 +187,8 @@ Java_org_nnsuite_nnstreamer_sample_MainActivity_nativeRunSample (JNIEnv * env, j
     return JNI_FALSE;
   }
 
-  /**
-   * Construct a pipeline.
-   */
-  status = ml_pipeline_construct (description, NULL, NULL, &pipe);
-  if (status != ML_ERROR_NONE) {
-    LOGE ("Failed to construct the pipeline.");
-    return JNI_FALSE;
-  }
-
-  /**
-   * Register sink callback.
-   */
-  status = ml_pipeline_sink_register (pipe, "sinkx", sample_sink_cb, NULL, &sink);
-  if (status != ML_ERROR_NONE) {
-    LOGE ("Failed to construct the pipeline.");
-    goto done;
-  }
-
-  /**
-   * Start the pipeline.
-   */
-  status = ml_pipeline_start (pipe);
-  if (status != ML_ERROR_NONE) {
-    LOGE ("Failed to start the pipeline.");
-    goto done;
-  }
-
-  /* sleep to run pipeline for a while */
-  sleep (5);
-
-done:
-  /**
-   * Destroy the pipeline.
-   */
-  if (ml_pipeline_destroy (pipe) != ML_ERROR_NONE) {
-    LOGE ("Failed to destroy pipeline handle.");
-  }
-
-  if (status != ML_ERROR_NONE) {
-    return JNI_FALSE;
-  }
+  run_simple_pipeline ();
+  run_snap_example ();
 
   return JNI_TRUE;
 }
