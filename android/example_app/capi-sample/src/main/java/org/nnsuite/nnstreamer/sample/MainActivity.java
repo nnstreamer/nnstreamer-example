195f3a051103e4518548cf287b7fc0eb3af177f5
Jaeyun <jy1210.jung@samsung.com>
2019-12-20 15:15:59 +0900

    [Android/SNAP] sample code for SNAP
    Update sample code to execute SNAP model.
    (native API with pipeline description)
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- android/example_app/capi-sample/src/main/java/org/nnsuite/nnstreamer/sample/MainActivity.java
+++ android/example_app/capi-sample/src/main/java/org/nnsuite/nnstreamer/sample/MainActivity.java
@@ -1,13 +1,24 @@
 package org.nnsuite.nnstreamer.sample;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import org.freedesktop.gstreamer.GStreamer;
 
 public class MainActivity extends Activity {
     private static final String TAG = "NNStreamer-Sample";
+
+    private static final int PERMISSION_REQUEST_CODE = 3;
+    private static final String[] requiredPermissions = new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     private native boolean nativeRunSample();
 
@@ -31,7 +42,49 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        /* Call native to run sample pipeline. */
+        /* Check permissions. */
+        for (String permission : requiredPermissions) {
+            if (!checkPermission(permission)) {
+                ActivityCompat.requestPermissions(this,
+                        requiredPermissions, PERMISSION_REQUEST_CODE);
+                return;
+            }
+        }
+
+        runSample();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission denied, close app.");
+                    finish();
+                    return;
+                }
+            }
+
+            runSample();
+            return;
+        }
+
+        finish();
+    }
+
+    /**
+     * Check the permission is granted.
+     */
+    private boolean checkPermission(final String permission) {
+        return (ContextCompat.checkSelfPermission(this, permission)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    /**
+     * Call native to run sample pipeline.
+     */
+    private void runSample() {
         if (!nativeRunSample()) {
             Log.e(TAG, "Failed to call native sample.");
         }
