583d2d62ad211169ebecbb8f5b64907cdf97ef47
gichan-jang <gichan2.jang@samsung.com>
2020-01-21 15:37:47 +0900

    [Tizen/.NET] Add text classification sample app
    Tizen .NET application with single-shot is added.
    Text classification tf-lite model is used.
    
    Todo: .NET app with pipeline API
    
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

---
+++ Tizen.NET/TextClassification/TextClassification/TextClassification.Tizen.Mobile/TextClassification.Tizen.Mobile.cs
@@ -0,0 +1,40 @@
+/**
+ * @file TextClassification.Tizen.Mobile.cs
+ * @date 13 JAN 2020
+ * @brief Text classification mobile app.
+ * @see  https://github.com/nnsuite/nnstreamer-example.git
+ * @author Gichan Jang <gichan2.jang@samsung.com>
+ * @bug No known bugs except for NYI items
+ *
+ */
+
+using System;
+namespace TextClassification.Tizen.Mobile
+{
+  /**
+  * @brief Represents xamarin forms of tizen platform app
+  */
+  class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+  {
+    /**
+    * @brief On create method
+    */
+    protected override void OnCreate()
+    {
+      base.OnCreate();
+      LoadApplication(new App());
+    }
+    /**
+    * @brief Main method of text classification tizen mobile
+    */
+    static void Main(string[] args)
+    {
+      var app = new Program();
+
+      /* Initiailize App */
+      global::Xamarin.Forms.Forms.Init(app);
+
+      app.Run(args);
+    }
+  }
+}
