45568c90b4bd90adf80a9fed5c977d6608cd8330
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2020-02-20 19:28:50 +0900

    [Ubuntu-Native/Example] Pose estimation example with TFLite model
    Single person pose estimation with public tflite model.
    It draws 17 body points of a person in input video stream. Details about the tflite model can be found here:
    https://www.tensorflow.org/lite/models/pose_estimation/overview
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- bash_script/example_models/get-model.sh
+++ bash_script/example_models/get-model.sh
@@ -3,14 +3,15 @@
 
 print_usage()
 {
-  echo "usage: $0 [model_name]"
-  echo "model_name     image-classification-tflite"
-  echo "               image-classification-caffe2"
-  echo "               object-detection-tf"
-  echo "               object-detection-tflite"
-  echo "               speech-command"
-  echo "               image-segmentation-tflite"
-  echo "               text-classification-tflite"
+  echo -e "usage: $0 [model_name]"
+  echo -e "model_name\timage-classification-tflite"
+  echo -e "\t\timage-classification-caffe2"
+  echo -e "\t\tobject-detection-tf"
+  echo -e "\t\tobject-detection-tflite"
+  echo -e "\t\tspeech-command"
+  echo -e "\t\timage-segmentation-tflite"
+  echo -e "\t\ttext-classification-tflite"
+  echo -e "\t\tpose-estimation-tflite"
   exit 1
 }
 
@@ -27,8 +28,7 @@ else
 fi
 
 
-
-if [ ${model} == image-classification-tflite ]; then
+if [[ ${model} == "image-classification-tflite" ]]; then
   mkdir -p tflite_model_img
   cd tflite_model_img
   wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/mobilenet_v1_1.0_224_quant_and_labels.zip
@@ -36,48 +36,59 @@ if [ ${model} == image-classification-tflite ]; then
   rm mobilenet_v1_1.0_224_quant_and_labels.zip
   mv labels_mobilenet_quant_v1_224.txt labels.txt
 
-elif [ ${model} == image-classification-caffe2 ]; then
+elif [[ ${model} == "image-classification-caffe2" ]]; then
   mkdir -p caffe2_model/tempdir
   cd caffe2_model/tempdir
   wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
   tar -zxf caffe_ilsvrc12.tar.gz
   mv synset_words.txt ../
   cd .. && rm -rf tempdir
-  wget https://s3.amazonaws.com/download.caffe2.ai/models/mobilenet_v2/predict_net.pb
-  wget https://s3.amazonaws.com/download.caffe2.ai/models/mobilenet_v2/init_net.pb
+  download_url="https://s3.amazonaws.com/download.caffe2.ai/models/mobilenet_v2"
+  wget ${download_url}/predict_net.pb
+  wget ${download_url}/init_net.pb
   mv synset_words.txt labels.txt
 
-elif [ ${model} == object-detection-tf ]; then
+elif [[ ${model} == "object-detection-tf" ]]; then
   mkdir -p tf_model
   cd tf_model
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow/ssdlite_mobilenet_v2/ssdlite_mobilenet_v2.pb
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow/ssdlite_mobilenet_v2/coco_labels_list.txt
+  download_url="https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow/ssdlite_mobilenet_v2"
+  wget ${download_url}/ssdlite_mobilenet_v2.pb
+  wget ${download_url}/coco_labels_list.txt
 
-elif [ ${model} == object-detection-tflite ]; then
+elif [[ ${model} == "object-detection-tflite" ]]; then
   mkdir -p tflite_model
   cd tflite_model
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/ssd_mobilenet_v2_coco.tflite
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/coco_labels_list.txt
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/box_priors.txt
+  download_url="https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco"
+  wget ${download_url}/ssd_mobilenet_v2_coco.tflite
+  wget ${download_url}/ssd_mobilenet_v2_coco/coco_labels_list.txt
+  wget ${download_url}/ssd_mobilenet_v2_coco/box_priors.txt
 
-elif [ ${model} == speech-command ]; then
+elif [[ ${model} == "speech-command" ]]; then
   mkdir -p speech_model
   cd speech_model
   wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/conv_actions_tflite.zip
   unzip conv_actions_tflite.zip
   rm conv_actions_tflite.zip
 
-elif [ ${model} ==  image-segmentation-tflite ]; then
+elif [[ ${model} == "image-segmentation-tflite" ]]; then
   mkdir -p tflite_img_segment_model
   cd tflite_img_segment_model
   wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/gpu/deeplabv3_257_mv_gpu.tflite
 
-elif [ ${model} == text-classification-tflite ]; then
+elif [[ ${model} == "text-classification-tflite" ]]; then
   mkdir -p tflite_text_classification
   cd tflite_text_classification
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/text_classification_tflite/text_classification.tflite
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/text_classification_tflite/labels.txt
-  wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/text_classification_tflite/vocab.txt
+  download_url="https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/text_classification_tflite"
+  wget ${download_url}/text_classification.tflite
+  wget ${download_url}/labels.txt
+  wget ${download_url}/vocab.txt
+
+elif [[ ${model} == "pose-estimation-tflite" ]]; then
+  mkdir -p tflite_pose_estimation
+  cd tflite_pose_estimation
+  wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite
+  echo -e "nose\nleftEye\nrightEye\nleftEar\nrightEar\nleftShoulder\nrightShoulder\nleftElbow\nrightElbow\nleftWrist\nrightWrist\nleftHip\nrightHip\nleftKnee\nrightKnee\nleftAnkle\nrightAnkle" > key_point_labels.txt
+
 else
   echo -e "${model}:not a valid model_name\n"
   print_usage
