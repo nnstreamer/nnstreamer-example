d75367f3464fb7f3b2b5601245c76f97a0ad89fe
gichan-jang <gichan2.jang@samsung.com>
2020-08-20 18:39:26 +0900

    [Openvino] Add face detection example using openvino
    Add face detection example using openvino
    
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

---
+++ bash_script/example_face_detection_openvino/gst-launch-face_detection_openvino.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+gst-launch-1.0 \
+    compositor name=mix sink_0::zorder=1 sink_1::zorder=2 ! videoconvert ! autovideosink sync=false \
+    v4l2src ! videoconvert ! videoscale ! video/x-raw,width=640,height=480,format=RGB,framerate=10/1 ! tee name=t \
+      t. ! queue ! mix.sink_0 \
+      t. ! queue leaky=2 max-size-buffers=2 ! videoconvert ! videoscale ! video/x-raw,height=300,width=300,format=BGR ! \
+        tensor_converter ! tensor_transform mode=typecast option=float32 ! tensor_transform mode=dimchg option=0:2 ! \
+        tensor_filter framework=openvino model=openvino_models/face-detection-retail-0005.xml accelerator=true:npu ! \
+        tensor_decoder mode=bounding_boxes option1=ov-face-detection option4=640:480 option5=300:300 ! mix.sink_1
