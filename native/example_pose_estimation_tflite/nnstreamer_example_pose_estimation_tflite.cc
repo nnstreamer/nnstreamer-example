8bcb16044a8ba61d544ab822c019be00bca0a927
Gichan Jang <gichan2.jang@samsung.com>
2022-01-07 16:29:22 +0900

    [Pose] Use decoder for pose estimation
    Use tensor decoder for pose estimation result
    
    Signed-off-by: Gichan Jang <gichan2.jang@samsung.com>

--- native/example_pose_estimation_tflite/nnstreamer_example_pose_estimation_tflite.cc
+++ native/example_pose_estimation_tflite/nnstreamer_example_pose_estimation_tflite.cc
@@ -4,19 +4,19 @@
  * @brief   Tensor Stream example with TensorFlow Lite model for pose estimation
  * @author  Yongjoo Ahn <yongjoo1.ahn@samsung.com>
  * @bug     No known bugs.
- * 
+ *
  * NNStreamer example for pose estimation on single person using
  * public TensorFlow Lite model in Ubuntu environment.
- * 
+ *
  * Get the required model by
  * $ cd $NNST_ROOT/bin
  * $ bash get-model.sh pose-estimation-tflite
- * 
+ *
  * It downloads the model from this link:
  * https://storage.googleapis.com/download.tensorflow.org/models/tflite/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite
  * and make text file of key point labels for this model (total 17 key points
  * include nose, left ear, right ankle, etc.)
- * 
+ *
  * Run example:
  * Before running this example, GST_PLUGIN_PATH should be updated for nnstreamer plug-in.
  * $ export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:<path/to/nnstreamer/plugin/path>
@@ -33,9 +33,6 @@
 #include <glib.h>
 #include <gst/gst.h>
 #include <gst/video/video.h>
-
-#include <math.h>
-#include <cairo.h>
 
 /**
  * @brief Macro for debug mode.
@@ -60,38 +57,12 @@
     } \
   } while (0)
 
-/**
- * @brief Macro for calculating sigmoid
- */
-#define _sigmoid(x) \
-    (1.f / (1.f + expf (-x)))
 
 #define VIDEO_HEIGHT        480
 #define VIDEO_WIDTH         640
 
 #define MODEL_INPUT_HEIGHT  257
 #define MODEL_INPUT_WIDTH   257
-
-#define KEYPOINT_SIZE       17
-#define OUTPUT_STRIDE       32
-#define GRID_YSIZE          9
-#define GRID_XSIZE          9
-
-#define SCORE_THRESHOLD     0.7
-
-typedef struct
-{
-  gchar *label;
-  gfloat y;
-  gfloat x;
-  gfloat score;
-} KeyPoint;
-
-typedef struct
-{
-  gboolean valid;
-  GstVideoInfo vinfo;
-} CairoOverlayState;
 
 /**
  * @brief Data structure for tflite model and label info.
@@ -100,7 +71,6 @@ typedef struct
 {
   gchar *model_path;
   gchar *label_path;
-  GList *labels;
 } tflite_info_s;
 
 /**
@@ -116,8 +86,6 @@ typedef struct
   guint received; /**< received buffer count */
   GMutex mutex; /**< mutex for processing */
   tflite_info_s tflite_info; /**< tflite models info */
-  KeyPoint kps[KEYPOINT_SIZE];
-  CairoOverlayState overlay_state;
 } AppData;
 
 /**
@@ -133,13 +101,12 @@ _tflite_init_info (tflite_info_s * tflite_info, const gchar * path)
 {
   const gchar tflite_model[] =
       "posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite";
-  const gchar tflite_label[] = "key_point_labels.txt";
+  const gchar tflite_label[] = "point_labels.txt";
 
   g_return_val_if_fail (tflite_info != NULL, FALSE);
 
   tflite_info->model_path = NULL;
   tflite_info->label_path = NULL;
-  tflite_info->labels = NULL;
 
   /* check model file can be loaded */
   tflite_info->model_path = g_strdup_printf ("%s/%s", path, tflite_model);
@@ -157,45 +124,7 @@ _tflite_init_info (tflite_info_s * tflite_info, const gchar * path)
     return FALSE;
   }
 
-  /* load labels */
-  FILE *fp = fopen (tflite_info->label_path, "r");
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
-  gchar *label;
-
-  while ((read = getline (&line, &len, fp)) != -1) {
-    line[read - 1] = '\0'; /**< trim the trailing newline chracter */
-    label = g_strdup ((gchar *) line);
-    tflite_info->labels = g_list_append (tflite_info->labels, label);
-  }
-
-  if (line) {
-    free (line);
-  }
-
-  fclose (fp);
-
-  _print_log ("Finished loading labels, total %d", g_list_length (tflite_info->labels));
-
   return TRUE;
-}
-
-/**
- * @brief Get label string with given index.
- */
-static gchar *
-_tflite_get_label (tflite_info_s * tflite_info, guint index)
-{
-  guint length;
-
-  g_return_val_if_fail (tflite_info != NULL, NULL);
-  g_return_val_if_fail (tflite_info->labels != NULL, NULL);
-
-  length = g_list_length (tflite_info->labels);
-  g_return_val_if_fail (index >= 0 && index < length, NULL);
-
-  return (gchar *) g_list_nth_data (tflite_info->labels, index);
 }
 
 /**
@@ -214,11 +143,6 @@ _tflite_free_info (tflite_info_s * tflite_info)
   if (tflite_info->label_path) {
     g_free (tflite_info->label_path);
     tflite_info->label_path = NULL;
-  }
-
-  if (tflite_info->labels) {
-    g_list_free_full (tflite_info->labels, g_free);
-    tflite_info->labels = NULL;
   }
 }
 
@@ -328,209 +252,6 @@ _message_cb (GstBus * bus, GstMessage * message, gpointer user_data)
 }
 
 /**
- * @brief Callback for tensor sink signal.
- */
-static void
-_new_data_cb (GstElement * element, GstBuffer * buffer, gpointer user_data)
-{
-  /* print progress */
-  g_app.received++;
-  _print_log ("receiving new data [%d]", g_app.received);
-
-  GstMemory *mem_heatmap, *mem_offset;
-  GstMapInfo info_heatmap, info_offset;
-  gfloat *heatmap, *offset;
-
-  g_return_if_fail (g_app.running);
-
-  /**
-   * tensor type is float32.
-   * [0] dim of heatmap := KEY_POINT_NUMBER : height_after_stride : 
-   *      width_after_stride: 1 (17:9:9:1)
-   * [1] dim of offsets := 34 (concat of y-axis and x-axis of offset vector) :
-   *      hegiht_after_stride : width_after_stride : 1 (34:9:9:1)
-   * [2] dim of displacement forward (not used for single person pose estimation)
-   * [3] dim of displacement backward (not used for single person pose estimation)
-   */ 
-
-  g_assert (gst_buffer_n_memory (buffer) == 4);
-
-  mem_heatmap = gst_buffer_get_memory (buffer, 0);
-  g_assert (gst_memory_map (mem_heatmap, &info_heatmap, GST_MAP_READ));
-  g_assert (info_heatmap.size == 17 * 9 * 9 * 1 * 4); // float32 is 4 bytes
-  heatmap = (gfloat *) info_heatmap.data; // can be viewed as float_arr[9][9][17]
-
-  mem_offset = gst_buffer_get_memory (buffer, 1);
-  g_assert (gst_memory_map (mem_offset, &info_offset, GST_MAP_READ));
-  g_assert (info_offset.size == 34 * 9 * 9 * 1 * 4);
-  offset = (gfloat *) info_offset.data; // can be viewd as float_arr[9][9][34]
-
-  for (gint keyPointIdx = 0; keyPointIdx < KEYPOINT_SIZE; ++keyPointIdx) {
-    gint yPosIdx = -1;
-    gint xPosIdx = -1;
-    gfloat highestScore = -G_MAXFLOAT;
-    gfloat currentScore;
-
-    gfloat yOffset, xOffset;
-    gfloat yPosition, xPosition;
-
-    /* find the index of key point with highestScore in 9 X 9 grid */
-    for (gint yIdx = 0; yIdx < GRID_YSIZE; ++yIdx) {
-      for (gint xIdx = 0; xIdx < GRID_XSIZE; ++xIdx) {
-        currentScore = _sigmoid (
-              heatmap[(yIdx * GRID_YSIZE + xIdx) * KEYPOINT_SIZE + keyPointIdx]);
-        if (currentScore > highestScore) {
-          yPosIdx = yIdx;
-          xPosIdx = xIdx;
-          highestScore = currentScore;
-        }
-      }
-    }
-
-    yOffset =
-        offset[(yPosIdx * GRID_YSIZE + xPosIdx) * KEYPOINT_SIZE * 2 + keyPointIdx];
-    xOffset =
-        offset[(yPosIdx * GRID_YSIZE + xPosIdx) * KEYPOINT_SIZE * 2 + KEYPOINT_SIZE + keyPointIdx];
-    
-    yPosition = ((gfloat) yPosIdx / (GRID_YSIZE - 1)) * MODEL_INPUT_HEIGHT + yOffset;
-    xPosition = ((gfloat) xPosIdx / (GRID_XSIZE - 1)) * MODEL_INPUT_WIDTH + xOffset;
-
-    g_app.kps[keyPointIdx].y = yPosition;
-    g_app.kps[keyPointIdx].x = xPosition;
-    g_app.kps[keyPointIdx].label = _tflite_get_label (&g_app.tflite_info, keyPointIdx);
-    g_app.kps[keyPointIdx].score = highestScore;   
-  }
-
-  gst_memory_unmap (mem_heatmap, &info_heatmap);
-  gst_memory_unmap (mem_offset, &info_offset);
-
-  gst_memory_unref (mem_heatmap);
-  gst_memory_unref (mem_offset);
-}
-
-/**
- * @brief Set window title.
- * @param name GstXImageSink element name
- * @param title window title
- */
-static void
-_set_window_title (const gchar * name, const gchar * title)
-{
-  GstTagList *tags;
-  GstPad *sink_pad;
-  GstElement *element;
-
-  element = gst_bin_get_by_name (GST_BIN (g_app.pipeline), name);
-
-  g_return_if_fail (element != NULL);
-
-  sink_pad = gst_element_get_static_pad (element, "sink");
-
-  if (sink_pad) {
-    tags = gst_tag_list_new (GST_TAG_TITLE, title, NULL);
-    gst_pad_send_event (sink_pad, gst_event_new_tag (tags));
-    gst_object_unref (sink_pad);
-  }
-
-  gst_object_unref (element);
-}
-
-/**
- * @brief Store the information from the caps that we are interested in.
- */
-static void
-prepare_overlay_cb (GstElement * overlay, GstCaps * caps, gpointer user_data)
-{
-  CairoOverlayState *state = &g_app.overlay_state;
-
-  state->valid = gst_video_info_from_caps (&state->vinfo, caps);
-}
-
-/**
- * @brief Draw line between key points
- */
-static void
-_draw_line (KeyPoint * kpts, cairo_t * cr, gint from, gint to)
-{
-  if (kpts[from].score < SCORE_THRESHOLD || kpts[to].score < SCORE_THRESHOLD)
-    return;
-
-  cairo_move_to (cr, kpts[from].x * VIDEO_WIDTH / MODEL_INPUT_WIDTH,
-      kpts[from].y * VIDEO_HEIGHT / MODEL_INPUT_HEIGHT);
-  cairo_line_to (cr, kpts[to].x * VIDEO_WIDTH / MODEL_INPUT_WIDTH,
-      kpts[to].y * VIDEO_HEIGHT / MODEL_INPUT_HEIGHT);
-}
-
-/**
- * @brief Callback to draw the overlay.
- */
-static void
-draw_overlay_cb (GstElement * overlay, cairo_t * cr, guint64 timestamp,
-    guint64 duration, gpointer user_data)
-{
-  CairoOverlayState *state = &g_app.overlay_state;
-  gfloat x, y, score;
-  // gchar *label;
-
-  g_return_if_fail (state->valid);
-  g_return_if_fail (g_app.running);
-  KeyPoint *kpts;
-  g_mutex_lock (&g_app.mutex);
-  kpts = g_app.kps;
-  g_assert (kpts);
-  g_mutex_unlock (&g_app.mutex);
-
-  /* set font props */
-  cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-      CAIRO_FONT_WEIGHT_BOLD);
-  cairo_set_font_size (cr, 8.0);
-  
-  for (gint keyPointIdx = 0; keyPointIdx < KEYPOINT_SIZE; ++keyPointIdx) {
-    score = kpts[keyPointIdx].score;
-    if (score < SCORE_THRESHOLD)
-      continue;
-    
-    y = kpts[keyPointIdx].y * VIDEO_HEIGHT / MODEL_INPUT_HEIGHT;
-    x = kpts[keyPointIdx].x * VIDEO_WIDTH / MODEL_INPUT_WIDTH;
-
-    /* draw key point */
-    cairo_set_source_rgb (cr, 0.85, 0.2, 0.2);
-    cairo_arc (cr, x, y, 3, 0, 2 * M_PI);
-    cairo_stroke_preserve (cr);
-    cairo_fill (cr);
-
-    /* draw the text of each key point */
-    cairo_move_to (cr, x + 5, y + 17);
-    cairo_text_path (cr, kpts[keyPointIdx].label);
-    cairo_set_source_rgb (cr, 0.7, 0.3, 0.5);
-    cairo_fill_preserve (cr);
-    cairo_set_source_rgb (cr, 0.7, 0.3, 0.5);
-    cairo_set_line_width (cr, .2);
-    cairo_stroke (cr);
-    cairo_fill_preserve (cr);
-  }
-
-  /* draw body lines */
-  cairo_set_source_rgb (cr, 0.85, 0.2, 0.2);
-  cairo_set_line_width (cr, 3);
-
-  _draw_line (kpts, cr, 5, 6);
-  _draw_line (kpts, cr, 5, 7);
-  _draw_line (kpts, cr, 7, 9);
-  _draw_line (kpts, cr, 6, 8);
-  _draw_line (kpts, cr, 8, 10);
-  _draw_line (kpts, cr, 6, 12);
-  _draw_line (kpts, cr, 5, 11);
-  _draw_line (kpts, cr, 11, 13);
-  _draw_line (kpts, cr, 13, 15);
-  _draw_line (kpts, cr, 12, 11);
-  _draw_line (kpts, cr, 12, 14);
-  _draw_line (kpts, cr, 14, 16);
-
-  cairo_stroke (cr);
-}
-
-/**
  * @brief Main function.
  */
 int
@@ -562,28 +283,17 @@ main (int argc, char ** argv)
       g_strdup_printf
       ("v4l2src name=src ! videoconvert ! videoscale ! "
       "video/x-raw,width=%d,height=%d,format=RGB ! tee name=t_raw "
-      "t_raw. ! queue ! videoconvert ! cairooverlay name=tensor_res ! "
-      "ximagesink name=img_tensor "
       "t_raw. ! queue leaky=2 max-size-buffers=2 ! videoscale ! "
       "video/x-raw,width=%d,height=%d,format=RGB ! tensor_converter ! "
       "tensor_transform mode=arithmetic option=typecast:float32,add:-127.5,div:127.5 ! "
       "tensor_filter framework=tensorflow-lite model=%s ! "
-      "tensor_sink name=tensor_sink",
+      "tensor_decoder mode=pose_estimation option1=640:480 option2=257:257 option3=%s option4=heatmap-offset ! "
+      "compositor name=mix sink_0::zorder=1 sink_1::zorder=0 ! videoconvert ! ximagesink "
+      "t_raw. ! queue ! mix.",
       VIDEO_WIDTH, VIDEO_HEIGHT, MODEL_INPUT_WIDTH, MODEL_INPUT_HEIGHT,
-      g_app.tflite_info.model_path);
+      g_app.tflite_info.model_path, g_app.tflite_info.label_path);
 
   _print_log ("%s\n", str_pipeline);
-
-  /**
-   * tensor info (posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite)
-   * 
-   * input[0] >> type:float32, dim[3:257:257:1], video stream (RGB 257x257)
-   * 
-   * output[0] >> type:float32, dim[17:9:9:1], heat map of 17 key points
-   * output[1] >> type:float32, dim[34:9:9:1], offset vectors
-   * output[2] >> type:float32, dim[32:9:9:1], not used
-   * output[3] >> type:float32, dim[32:9:9:1], not used
-   */
 
   g_app.pipeline = gst_parse_launch (str_pipeline, NULL);
   g_free (str_pipeline);
@@ -597,25 +307,9 @@ main (int argc, char ** argv)
   handle_id = g_signal_connect (g_app.bus, "message", G_CALLBACK (_message_cb), NULL);
   _check_cond_err (handle_id > 0);
 
-  /* tensor sink signal : new data callback */
-  element = gst_bin_get_by_name (GST_BIN (g_app.pipeline), "tensor_sink");
-  handle_id = g_signal_connect (element, "new-data", G_CALLBACK (_new_data_cb), NULL);
-  gst_object_unref (element);
-  _check_cond_err (handle_id > 0);
-
-  /* cairo overlay */
-  element = gst_bin_get_by_name (GST_BIN (g_app.pipeline), "tensor_res");
-  g_signal_connect (element, "draw", G_CALLBACK (draw_overlay_cb), NULL);
-  g_signal_connect (element, "caps-changed", G_CALLBACK (prepare_overlay_cb), NULL);
-  gst_object_unref (element);
-
   /* start pipeline */
   gst_element_set_state (g_app.pipeline, GST_STATE_PLAYING);
   g_app.running = TRUE;
-
-  /* set window title */
-  _set_window_title ("img_tensor",
-      "An NNStreamer Example - Single Person Pose Estimation");
 
   /* run main loop */
   g_main_loop_run (g_app.loop);
--- native/example_pose_estimation_tflite/nnstreamer_example_pose_estimation_tflite.py
+++ native/example_pose_estimation_tflite/nnstreamer_example_pose_estimation_tflite.py
@@ -33,13 +33,8 @@ import os
 import sys
 import gi
 import logging
-import math
-import numpy as np
-import ctypes
-import cairo
 
 gi.require_version('Gst', '1.0')
-gi.require_foreign('cairo')
 from gi.repository import Gst, GObject
 
 DEBUG = False
@@ -50,24 +45,15 @@ class NNStreamerExample:
     def __init__(self, argv=None):
         self.loop = None
         self.pipeline = None
-        self.running = False
-        self.video_caps = None
 
         self.VIDEO_WIDTH = 640
         self.VIDEO_HEIGHT = 480
         self.MODEL_INPUT_HEIGHT = 257
         self.MODEL_INPUT_WIDTH = 257
 
-        self.KEYPOINT_SIZE = 17
-        self.OUTPUT_STRIDE = 32
-        self.GRID_XSIZE = 9
-        self.GRID_YSIZE = 9
-
-        self.SCORE_THRESHOLD = 0.7
 
         self.tflite_model = ''
-        self.tflite_labels = []
-        self.kps = []
+        self.label_path = ''
 
         if not self.tflite_init():
             raise Exception
@@ -95,7 +81,9 @@ class NNStreamerExample:
             'video/x-raw,width=' + str(self.MODEL_INPUT_WIDTH) + ',height=' + str(self.MODEL_INPUT_HEIGHT) + ',format=RGB ! tensor_converter ! '
             'tensor_transform mode=arithmetic option=typecast:float32,add:-127.5,div:127.5 ! '
             'tensor_filter framework=tensorflow-lite model=' + self.tflite_model + ' ! '
-            'tensor_sink name=tensor_sink'
+            'tensor_decoder mode=pose_estimation option1=640:480 option2=257:257 option3=' +self.label_path + ' option4=heatmap-offset ! '
+            'compositor name=mix sink_0::zorder=1 sink_1::zorder=0 ! videoconvert ! ximagesink '
+            't_raw. ! queue ! mix.'
         )
 
         # bus and message callback
@@ -103,25 +91,13 @@ class NNStreamerExample:
         bus.add_signal_watch()
         bus.connect('message', self.on_bus_message)
 
-        # tensor sink signal : new data callback
-        tensor_sink = self.pipeline.get_by_name('tensor_sink')
-        tensor_sink.connect('new-data', self.new_data_cb)
-
-        tensor_res = self.pipeline.get_by_name('tensor_res')
-        tensor_res.connect('draw', self.draw_overlay_cb)
-        tensor_res.connect('caps-changed', self.prepare_overlay_cb)
-
         # start pipeline
         self.pipeline.set_state(Gst.State.PLAYING)
-        self.running = True
-
-        self.set_window_title('img_tensor', 'An NNStreamer Example - Single Person Pose Estimation')
 
         # run main loop
         self.loop.run()
 
         # quit when received eos or error message
-        self.running = False
         self.pipeline.set_state(Gst.State.NULL)
 
         bus.remove_signal_watch()
@@ -131,7 +107,7 @@ class NNStreamerExample:
         :return: True if successfully initialized
         """
         tflite_model = 'posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite'
-        tflite_label = 'key_point_labels.txt'
+        tflite_label = 'point_labels.txt'
 
         current_folder = os.path.dirname(os.path.abspath(__file__))
         model_folder = os.path.join(current_folder, 'tflite_pose_estimation')
@@ -141,148 +117,13 @@ class NNStreamerExample:
             logging.error('cannot find tflite model [%s]', self.tflite_model)
             return False
 
-        label_path = os.path.join(model_folder, tflite_label)
-        try:
-            with open(label_path, 'r') as label_file:
-                for line in label_file.readlines():
-                    self.tflite_labels.append(line)
-        except FileNotFoundError:
-            logging.error('cannot find tflite label [%s]', label_path)
+        self.label_path = os.path.join(model_folder, tflite_label)
+        if not os.path.exists(self.label_path):
+            logging.error('cannot find point label [%s]', self.label_path)
             return False
 
-        logging.info('finished to load labels, total [%d]', len(self.tflite_labels))
         return True
 
-    # @brief Callback for tensor sink signal.
-    def new_data_cb(self, sink, buffer):
-        if self.running:
-            if buffer.n_memory() != 4:
-                return False
-            #  tensor type is float32.
-            #  [0] dim of heatmap := KEY_POINT_NUMBER : height_after_stride : 
-            #       width_after_stride: 1 (17:9:9:1)
-            #  [1] dim of offsets := 34 (concat of y-axis and x-axis of offset vector) :
-            #       hegiht_after_stride : width_after_stride : 1 (34:9:9:1)
-            #  [2] dim of displacement forward (not used for single person pose estimation)
-            #  [3] dim of displacement backward (not used for single person pose estimation)
-
-            # heatmap
-            mem_heatmap = buffer.peek_memory(0)
-            result1, info_heatmap = mem_heatmap.map(Gst.MapFlags.READ)
-            if result1:
-                assert info_heatmap.size == 17 * 9 * 9 * 1 * 4
-                decoded_heatmap = list(np.frombuffer(info_heatmap.data, dtype=np.float32))  # decode bytestrings to float list
-            
-            # offset
-            mem_offset = buffer.peek_memory(1)
-            result2, info_offset = mem_offset.map(Gst.MapFlags.READ)
-            if result2:
-                assert info_offset.size == 34 * 9 * 9 * 1 * 4
-                decoded_offset = list(np.frombuffer(info_offset.data, dtype=np.float32)) # decode bytestrings to float list
-            
-            self.kps.clear()
-            for keyPointIdx in range(0, self.KEYPOINT_SIZE):
-                yPosIdx = -1
-                xPosIdx = -1
-                highestScore = -float("1.0842021724855044e-19")
-                currentScore = 0
-
-                # find the index of key point with highestScore in 9 X 9 grid
-                for yIdx in range(0, self.GRID_YSIZE):
-                    for xIdx in range(0, self.GRID_XSIZE):
-                        current = decoded_heatmap[(yIdx * self.GRID_YSIZE + xIdx) * self.KEYPOINT_SIZE + keyPointIdx]
-                        currentScore = 1.0 / (1.0 + math.exp(-current))
-                        if(currentScore > highestScore):
-                            yPosIdx = yIdx
-                            xPosIdx = xIdx
-                            highestScore = currentScore
-
-                yOffset = decoded_offset[(yPosIdx * self.GRID_YSIZE + xPosIdx) * self.KEYPOINT_SIZE * 2 + keyPointIdx]
-                xOffset = decoded_offset[(yPosIdx * self.GRID_YSIZE + xPosIdx) * self.KEYPOINT_SIZE * 2 + self.KEYPOINT_SIZE + keyPointIdx]
-
-                yPosition = (yPosIdx / (self.GRID_YSIZE - 1)) * self.MODEL_INPUT_HEIGHT + yOffset
-                xPosition = (xPosIdx / (self.GRID_XSIZE - 1)) * self.MODEL_INPUT_WIDTH + xOffset
-
-                obj = {
-                    'y': yPosition,
-                    'x': xPosition,
-                    'label': keyPointIdx,
-                    'score': highestScore
-                }
-
-                self.kps.append(obj)
-
-            mem_heatmap.unmap(info_heatmap)
-            mem_offset.unmap(info_offset)
-
-    # @brief Store the information from the caps that we are interested in.
-    def prepare_overlay_cb(self, overlay, caps):
-        self.video_caps = caps
-
-    # @brief Callback to draw the overlay.
-    def draw_overlay_cb(self, overlay, context, timestamp, duration):
-        if self.video_caps == None or not self.running:
-            return
-
-        # mutex_lock alternative required
-        kpts = self.kps
-        # mutex_unlock alternative needed
-        
-        context.select_font_face('Sans', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-        context.set_font_size(8.0)
-
-        for obj in kpts:
-            score = obj['score']
-            if score < self.SCORE_THRESHOLD:
-                continue
-
-            label = self.tflite_labels[obj['label']][:-1]
-            y = obj['y'] * self.VIDEO_HEIGHT / self.MODEL_INPUT_HEIGHT
-            x = obj['x'] * self.VIDEO_WIDTH / self.MODEL_INPUT_WIDTH
-
-            # draw key point
-            context.set_source_rgb(0.85, 0.2, 0.2)
-            context.arc(x, y, 3, 0, 2 * math.pi)
-            context.stroke_preserve()
-            context.fill()
-
-            # draw the text of each key point
-            context.move_to(x + 5, y + 17)
-            context.text_path(label)
-            context.set_source_rgb(0.7, 0.3, 0.5)
-            context.fill_preserve()
-            context.set_source_rgb(0.7, 0.3, 0.5)
-            context.set_line_width(0.2)
-            context.stroke()
-            context.fill_preserve()
-
-        # draw body lines
-        context.set_source_rgb(0.85, 0.2, 0.2)
-        context.set_line_width(3)
-
-        self.draw_line(overlay, context, kpts, 5, 6)
-        self.draw_line(overlay, context, kpts, 5, 7)
-        self.draw_line(overlay, context, kpts, 7, 9)
-        self.draw_line(overlay, context, kpts, 6, 8)
-        self.draw_line(overlay, context, kpts, 8, 10)
-        self.draw_line(overlay, context, kpts, 6, 12)
-        self.draw_line(overlay, context, kpts, 5, 11)
-        self.draw_line(overlay, context, kpts, 11, 13)
-        self.draw_line(overlay, context, kpts, 13, 15)
-        self.draw_line(overlay, context, kpts, 12, 11)
-        self.draw_line(overlay, context, kpts, 12, 14)
-        self.draw_line(overlay, context, kpts, 14, 16)
-
-        context.stroke()
-
-    def draw_line(self, overlay, context, kpts, from_key, to_key):
-        if len(kpts) < 1:
-            return
-        if kpts[from_key]['score'] < self.SCORE_THRESHOLD or kpts[to_key]['score'] < self.SCORE_THRESHOLD:
-            return
-        
-        context.move_to(kpts[from_key]['x'] * self.VIDEO_WIDTH / self.MODEL_INPUT_WIDTH, kpts[from_key]['y'] * self.VIDEO_HEIGHT / self.MODEL_INPUT_HEIGHT)
-        context.line_to(kpts[to_key]['x'] * self.VIDEO_WIDTH / self.MODEL_INPUT_WIDTH, kpts[to_key]['y'] * self.VIDEO_HEIGHT / self.MODEL_INPUT_HEIGHT)
 
     def on_bus_message(self, bus, message):
         """Callback for message.
@@ -324,5 +165,3 @@ class NNStreamerExample:
 if __name__ == '__main__':
     example = NNStreamerExample(sys.argv[1:])
     example.run_example()
-
-
