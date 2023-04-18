9454b2793835a5da42a075cde35626125cd4e461
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2023-04-18 17:48:51 +0900

    [ml-svc-model] Add a tpk providing mobilenetv2 model
    - When this application is launched, it register the mobilenetv2 model
    - imgcls app could use this app w/o code change
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

---
+++ Tizen.native/ml-svc-model/mobilenetv2/inc/mobilenetv2.h
@@ -0,0 +1,19 @@
+#ifndef __mobilenetv2_H__
+#define __mobilenetv2_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "mobilenetv2"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.mobilenetv2"
+#endif
+
+#endif /* __mobilenetv2_H__ */
