feab4fdee5d0a963b29a545945216f3adc8e39b7
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2023-09-07 14:56:31 +0900

    [bash] Fix yolov5 pipeline
    - Fix 85:6300:1:1 -> 85:6300:1 corresponding recent NNStreamer's
      dimension policy
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- bash_script/example_yolo/gst-launch-object-detection-yolov5-tflite.sh
+++ bash_script/example_yolo/gst-launch-object-detection-yolov5-tflite.sh
@@ -11,11 +11,11 @@ gst-launch-1.0 \
     video/x-raw,width=320,height=320,format=RGB ! tensor_converter ! \
     tensor_transform mode=arithmetic option=typecast:float32,div:255.0 ! \
     queue ! tensor_filter framework=tensorflow2-lite model=yolov5s-fp16.tflite custom=Delegate:XNNPACK,NumThreads:4 ! \
-    other/tensors,num_tensors=1,types=float32,dimensions=85:6300:1:1,format=static ! \
+    other/tensors,num_tensors=1,types=float32,dimensions=85:6300:1,format=static ! \
     tensor_decoder mode=bounding_boxes option1=yolov5 option2=coco.txt option3=0 option4=1000:1000 option5=320:320 ! \
     video/x-raw,width=1000,height=1000,format=RGBA ! mix.sink_0 \
   t. ! queue leaky=2 max-size-buffers=10 ! mix.sink_1 \
-  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! ximagesink sync=false
+  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! autovideosink sync=false
 
 else
   echo "Use quantized tflite model"
@@ -27,12 +27,12 @@ gst-launch-1.0 \
   t. ! queue leaky=2 max-size-buffers=2 ! videoscale ! \
     video/x-raw,width=320,height=320,format=RGB ! tensor_converter ! \
     queue ! tensor_filter framework=tensorflow2-lite model=yolov5s-int8.tflite custom=Delegate:GPU ! \
-    other/tensors,num_tensors=1,types=uint8,dimensions=85:6300:1:1,format=static ! \
+    other/tensors,num_tensors=1,types=uint8,dimensions=85:6300:1,format=static ! \
     tensor_transform mode=arithmetic option=typecast:float32,add:-4.0,mul:0.0051498096 ! \
     queue ! tensor_decoder mode=bounding_boxes option1=yolov5 option2=coco.txt option3=0 option4=1000:1000 option5=320:320 ! \
     video/x-raw,width=1000,height=1000,format=RGBA ! mix.sink_0 \
   t. ! queue leaky=2 max-size-buffers=10 ! mix.sink_1 \
-  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! ximagesink sync=false
+  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! autovideosink sync=false
 
 ## Note that the input is uint8 input and the output of the tensor_filter is dequantized by
 ##  tensor_transform mode=arithmetic option=typecast:float32,add:-4.0,mul:0.0051498096
