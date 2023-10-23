af6d9bc6a07faebc703069cca4d894236395c6b3
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2023-10-23 15:15:09 +0900

    [bash] Fix audio classification example script
    - Handle recent NNStreamer's update on rank.
      (Use transpose rather than dimchg)
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- bash_script/example_audio_classification/gst-launch-audio-classification.sh
+++ bash_script/example_audio_classification/gst-launch-audio-classification.sh
@@ -16,12 +16,11 @@ unzip yamnet_classification.tflite
 #   - get argmax of the output and get the corresponding label. note that the confidence is not considered.
 #   - display the label via textoverlay
 gst-launch-1.0 \
-  alsasrc device=hw:2 ! audioconvert ! audio/x-raw,format=S16LE,channels=1,rate=16000,layout=interleaved ! \
+  alsasrc ! audioconvert ! audio/x-raw,format=S16LE,channels=1,rate=16000,layout=interleaved ! \
     tensor_converter frames-per-tensor=3900 ! \
     tensor_aggregator frames-in=3900 frames-out=15600 frames-flush=3900 frames-dim=1 ! \
     tensor_transform mode=arithmetic option=typecast:float32,add:0.5,div:32767.5 ! \
-    tensor_transform mode=dimchg option=0:1 ! \
-    other/tensors,num_tensors=1,dimensions=15600:1:1:1,types=float32,format=static ! \
+    tensor_transform mode=transpose option=1:0:2:3 ! \
     queue leaky=2 max-size-buffers=10 ! tensor_filter framework=tensorflow2-lite model=yamnet_classification.tflite custom=Delegate:XNNPACK,NumThreads:4 ! \
     tensor_decoder mode=image_labeling option1=yamnet_label_list.txt ! overlay.text_sink \
   videotestsrc ! videoconvert ! videoscale ! video/x-raw,width=640,height=480,format=RGB ! queue ! overlay.video_sink \
