e81dff36e82d59121864f289200f8afa31658d13
Yelin Jeong <yelini.jeong@samsung.com>
2024-07-01 17:24:56 +0900

    [Tizen/Service] Add native service for hybrid application
    This patch adds Tizen native service.
    Native service application will be used for hybrid web application.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

---
+++ Tizen.web/ImageClassificationOffloadingService/inc/imageclassificationoffloadingservice.h
@@ -0,0 +1,12 @@
+#ifndef __IMAGECLASSIFICATIONOFFLOADINGSERVICE_H__
+#define __IMAGECLASSIFICATIONOFFLOADINGSERVICE_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "imageclassificationoffloadingservice"
+
+
+#endif /* __IMAGECLASSIFICATIONOFFLOADINGSERVICE_H__ */
