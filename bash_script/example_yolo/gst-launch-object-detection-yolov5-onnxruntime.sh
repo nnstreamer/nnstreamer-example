ceb64b861ed91e9fd08ad7c7fca5e25160621079
Bram Veldhoen <bram@adiplus.nl>
2024-08-08 17:48:38 +0200

    Add tensorrt10 example
    Adds an example using tensor_filter framework=tensorrt10.
    
    Signed-off-by: Bram Veldhoen <bram@adiplus.nl>

--- bash_script/example_yolo/gst-launch-object-detection-yolov5-onnxruntime.sh
+++ bash_script/example_yolo/gst-launch-object-detection-yolov5-onnxruntime.sh
@@ -8,7 +8,7 @@ gst-launch-1.0 \
     video/x-raw,width=320,height=320,format=RGB ! tensor_converter ! \
     tensor_transform mode=transpose option=1:2:0:3 ! \
     tensor_transform mode=arithmetic option=typecast:float32,div:255.0 ! \
-    queue ! tensor_filter framework=pytorch model=yolov5su.torchscript input=320:320:3:1 inputtype=float32 output=2100:84:1 outputtype=float32 latency=1 ! \
+    queue ! tensor_filter framework=onnxruntime model=yolov5su.onnx input=320:320:3:1 inputtype=float32 output=2100:84:1 outputtype=float32 latency=1 ! \
     tensor_transform mode=transpose option=1:0:2:3 ! \
     other/tensors,num_tensors=1,types=float32,dimensions=84:2100:1,format=static ! \
     tensor_decoder mode=bounding_boxes option1=yolov8 option2=coco.txt option3=1 option4=1000:1000 option5=320:320 ! \
