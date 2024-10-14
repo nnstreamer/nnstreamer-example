0a33d4e37af938269a8ef0a1845622148a5444a8
Yelin Jeong <yelini.jeong@samsung.com>
2024-10-14 14:14:42 +0900

    [Tizen/web] Add text generation web application
    This patch adds tizen web application template from tizen studio.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

---
+++ Tizen.web/TextGenerationLlama2/WebApplication/js/main.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    // TODO:: Do your initialization job
+
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === "back") {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+
+    // Sample code
+    var mainPage = document.querySelector('#main');
+
+    mainPage.addEventListener("click", function() {
+        var contentText = document.querySelector('#content-text');
+
+        contentText.innerHTML = (contentText.innerHTML === "Basic") ? "Tizen" : "Basic";
+    });
+};
