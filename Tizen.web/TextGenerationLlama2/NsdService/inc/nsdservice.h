a673394b7ab06a1f0b4e312cfe8afa501253c2d9
Yelin Jeong <yelini.jeong@samsung.com>
2024-10-18 15:58:04 +0900

    [Tizen/native] Add nsd service for text offloading
    This patch adds nsd service.
    Tizen only has native nsd related api,
    so native service is needed to find ip and port.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

---
+++ Tizen.web/TextGenerationLlama2/NsdService/inc/nsdservice.h
@@ -0,0 +1,21 @@
+/**
+ * @file	main.h
+ * @date	04 Jul 2024
+ * @brief	Tizen native service for hybrid application
+ * @author	Yelin Jeong <yelini.jeong@samsung.com>
+ * @bug		No known bugs.
+ */
+
+#ifndef __NSD_SERVICE_H__
+#define __NSD_SERVICE_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "nsd_service"
+
+#define REMOTE_APP_ID "lfebC6UrMY.Llama2"
+
+#endif /* __NSD_SERVICE_H__ */
